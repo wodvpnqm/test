3d60ac04598af7f57b47a694963e1781d166f020
wodvpn <wodvpn@wodvpn-PC>
2017-06-30 23:50:54 +0800

    d

---
+++ src/main/java/com/qm/algorithms/algorithm605/Solution.java
@@ -0,0 +1,21 @@
+package com.qm.algorithms.algorithm605;
+
+public class Solution {
+    /**
+     * 605. Can Place Flowers
+     * 
+     * 假设你有一个长的花坛，其中一些地块种植，有些不是。 然而，花朵不能种植在相邻的地块上 - 他们会争取水，两者都会死亡。
+     * 
+     * 给定一个花坛（表示为包含0和1的数组，其中0表示空，1表示不为空），数字n返回，
+     * 如果n个新鲜花可以种植在其中，而不违反不相邻花规则。
+     * 
+     * 
+     * 
+     * @param flowerbed
+     * @param n
+     * @return
+     */
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        return false;
+    }
+}
