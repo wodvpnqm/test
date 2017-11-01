99db48c7a4fabe03a422a2d3220ee964bf4de952
wodvpn <qiumiaod@126.com>
2017-11-01 16:36:26 +0800

    jvm 内存

---
+++ src/main/java/com/qm/jvm/memory/JavaMethodAreaOOM.java
@@ -0,0 +1,33 @@
+package com.qm.jvm.memory;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * 借助CGLib使方法区出现内存溢出异常
+ * VM Args:-XX:PermSize=10M -XX:MaxPermSize=10M
+ *
+ *
+ *
+ */
+public class JavaMethodAreaOOM {
+    public static void main(String[] args) {
+        while(true){
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor(){
+                @Override
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                    return methodProxy.invokeSuper(o,objects);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject{}
+}
