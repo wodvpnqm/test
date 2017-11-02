b48cf85db7278cbf8ed1a0b7f346c23260324a26
wodvpn <qiumiaod@126.com>
2017-11-02 17:05:17 +0800

    jvm 多线程

---
+++ src/main/java/com/qm/jvm/thread/VolatileTest.java
@@ -0,0 +1,31 @@
+package com.qm.jvm.thread;
+
+public class VolatileTest {
+
+    public static volatile int race = 0;
+
+    public static void increase(){
+        race++;
+    }
+
+    private static  final int THREAD_COUNT = 20;
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREAD_COUNT];
+        for(int i=0;i<THREAD_COUNT;i++){
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i=0;i<10000;i++){
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+        while(Thread.activeCount() > 1){
+            Thread.yield();
+        }
+        System.out.println(race);
+    }
+}
